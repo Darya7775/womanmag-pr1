0526da13d19ee2db72da2b8a226f25a0bef43d0f
unknown <stolyarova.199535@gmail.com>
2023-02-08 18:52:31 +0300

    mмодальные окна, главный экран

--- source/components/form-validate/js/index.js
+++ source/components/form-validate/js/index.js
@@ -1,12 +1,12 @@
-import {CustomSelect} from './select/custom-select';
-import {Form} from './form-validate/form';
+// import {CustomSelect} from './select/custom-select';
+// import {Form} from './form-validate/form';
 
-window.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('load', () => {
-    const select = new CustomSelect();
-    select.init();
-    const form = new Form();
-    window.form = form;
-    form.init();
-  });
-});
+// window.addEventListener('DOMContentLoaded', () => {
+//   window.addEventListener('load', () => {
+//     const select = new CustomSelect();
+//     select.init();
+//     const form = new Form();
+//     window.form = form;
+//     form.init();
+//   });
+// });
--- source/js/main.js
+++ source/js/main.js
@@ -1,19 +1,21 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initSandwichMenu} from './modules/init-sandwich-menu';
-import {ScrollLock} from './utils/scroll-lock';
-import {initDynamicAdaptive} from './modules/init-dynamic-adaptive';
-import {initAccordions} from './modules/init-accordion';
-import {initUpButton} from './modules/init-up-button';
-import {initMoveTo} from './modules/init-move-to';
-import {initNavbarMenu} from './modules/init-navbar-menu';
-import {initHeaderObserver} from './modules/init-header-observer';
-import {initTabs} from './modules/init-tabs';
-import {initNavigationChanger} from './modules/init-navigation-changer';
-import {initArticleMenu} from './modules/init-article-menu';
-import {initChangeNumber} from './modules/init-change-number';
-import {initFixedNavbar} from './modules/init-fixed-navbar';
-import {initYearCorrection} from './modules/init-year-correction';
+// import {iosVhFix} from './utils/ios-vh-fix';
+// import {initSandwichMenu} from './modules/init-sandwich-menu';
+// import {ScrollLock} from './utils/scroll-lock';
+// import {initDynamicAdaptive} from './modules/init-dynamic-adaptive';
+// import {initAccordions} from './modules/init-accordion';
+// import {initUpButton} from './modules/init-up-button';
+// import {initMoveTo} from './modules/init-move-to';
+// import {initNavbarMenu} from './modules/init-navbar-menu';
+// import {initHeaderObserver} from './modules/init-header-observer';
+// import {initTabs} from './modules/init-tabs';
+// import {initNavigationChanger} from './modules/init-navigation-changer';
+// import {initArticleMenu} from './modules/init-article-menu';
+// import {initChangeNumber} from './modules/init-change-number';
+// import {initFixedNavbar} from './modules/init-fixed-navbar';
+// import {initYearCorrection} from './modules/init-year-correction';
 import {initModals} from './modules/init-modals';
+import {Form} from './utils/form/form';
+import Swiper, {Pagination} from 'swiper';
 // import {initSearch} from './modules/init-search.js';
 
 
@@ -23,28 +25,40 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Utils
   // ---------------------------------
-  initDynamicAdaptive();
-  window.scrollLock = new ScrollLock();
-  iosVhFix();
-  initHeaderObserver();
-  initYearCorrection();
+  // initDynamicAdaptive();
+  // window.scrollLock = new ScrollLock();
+  const form = new Form();
+  window.form = form;
+  // iosVhFix();
+  // initHeaderObserver();
+  // initYearCorrection();
+
+  void new Swiper('.swiper', {
+    modules: [Pagination],
+    pagination: {
+      el: '.main__pagination',
+      clickable: true,
+    },
+  });
+
   // Modules
   // ---------------------------------
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initSandwichMenu();
-    initAccordions();
-    initUpButton();
-    initMoveTo();
-    initNavbarMenu();
-    initTabs();
-    initNavigationChanger();
-    initArticleMenu();
-    initChangeNumber();
-    initFixedNavbar();
+    // initSandwichMenu();
+    // initAccordions();
+    // initUpButton();
+    // initMoveTo();
+    // initNavbarMenu();
+    // initTabs();
+    // initNavigationChanger();
+    // initArticleMenu();
+    // initChangeNumber();
+    // initFixedNavbar();
     initModals();
+    form.init();
     // initSearch();
   });
 });
